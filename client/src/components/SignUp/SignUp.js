d21130ba95c7df419822db1a7482aef0e09ee0c2
salt-dev <loukasmalias@hotmail.com>
2021-12-02 10:39:08 +0100

    signup form implemented

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -11,19 +11,27 @@ const SignUp = () => {
 
   return (
     <>
-      <h2 className="page-content__login-title">Login</h2>
-      <form className="page-content__login-form" onSubmit={handleSubmit}>
-        <label className="login-form__label">
+      <h2 className="page-content__join-title">Sign Up</h2>
+      <form className="page-content__join-form" onSubmit={handleSubmit}>
+        <label className="join-form__label">
+          Username
+          <input className="join-form__input" type="text" placeholder="Username" required />
+        </label>
+        <label className="join-form__label">
           Email
-          <input className="login-form__input" type="email" placeholder="example@email.com" required />
+          <input className="join-form__input" type="email" placeholder="example@email.com" required />
         </label>
-        <label className="login-form__label">
+        <label className="join-form__label">
           Password
-          <input className="login-form__input" type="password" placeholder="Password" required />
+          <input className="join-form__input" type="password" placeholder="Password" required />
         </label>
-        <button className="login-form__button" type="submit">Join</button>
+        <label className="join-form__label">
+          Confirm Password
+          <input className="join-form__input" type="password" placeholder="Confirm Password" required />
+        </label>
+        <button className="join-form__button" type="submit">Sign Up</button>
       </form>
-      <p className="page-content__login">Already a member? <Link className="login__link" to='/login'>Login</Link></p>
+      <p className="page-content__sign-in">Already a member? <Link className="sign-in__link" to='/login'>Login</Link></p>
     </>
   );
 };
